d6837b914d23e64e6f5aa20f5c0e8644150ec33e
JamerRivera <perezfurina5@gmail.com>
2024-09-11 12:37:00 -0500

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,25 @@
+
+import formularios.persona;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author jamer
+ */
+public class MAIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        persona J = new persona();
+        J.setVisible(true);
+        
+        
+    }
+    
+}
